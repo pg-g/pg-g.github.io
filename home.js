08ad6897328da8a45eb8b453ca8e335b809f90a5
pg-g <paigegu3+git@gmail.com>
2016-08-22 00:30:48 -0600

    2016 updates

---
+++ home.js
@@ -0,0 +1,15 @@
+$('#blue-btn').on("click", function(){
+	$('.color').css('background-color','#58C1EF');
+});
+$('#gold-btn').on("click", function(){
+	$('.color').css('background-color','#FFCB28');
+});
+$('#orange-btn').on("click", function(){
+	$('.color').css('background-color','#F58967');
+});
+$('#gray-btn').on("click", function(){
+	$('.color').css('background-color','#58595B');
+});
+$('#dark-btn').on("click", function(){
+	$('.color').css('background-color','#386B95');
+});
